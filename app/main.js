860921cd00520eb964290781dd5d7724ef2ea01c
Jonathan Wright <j.wright72127@gmail.com>
2021-07-13 14:04:41 -0600

    Cleaned up unnecessary code

--- app/main.js
+++ app/main.js
@@ -1,9 +1,6 @@
 import MachinesController from "./Controllers/MachinesController.js";
-import ValuesController from "./Controllers/ValuesController.js";
 
 class App {
-  valuesController = new ValuesController();
-
   machinesController = new MachinesController();
 }
 
